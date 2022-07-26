cd952e108c9e2cd33a9f12ab757cd39aedc2c0e3
Borhan Boulandier <borhan.boulandier@gmail.com>
2022-07-26 12:30:17 +0100

    Refactor, reusable function to populate country card HTML

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
+// Create country card HTML function
+function createCountryCard(obj) {
+  const element = `
+  <div class="col-3">
+    <div class="country-card my-2">
+      <img id="flag" src="${obj.flags.png}" alt="">
+      <div class="country-card-body">
+        <h2 id="name">${obj.name.common}</h2>
+        <h4>Population: <span id="population">${obj.population}</span></h4>
+        <h4>Capital: <span id="capital">${obj.capital}</span></h4>
+        <h4>Continent: <span id="continent">${obj.continents[0]}</span></h4>
+      </div>
+    </div>
+  </div>
+  `
+  return element;
+}
+
 // Select row to insert country cards
 const countries = document.getElementById("countries")
 
@@ -6,35 +24,29 @@ fetch("https://restcountries.com/v3.1/all")
   .then(response => response.json())
   .then((data) => {
 
-    // Iterate through all countries and create HTML populated with country API DATA
+    // Iterate through all countries and create HTML populated with country API data
     Object.values(data).forEach((country) => {
-      const countryCard = `
-        <div class="col-3">
-          <div class="country-card my-2">
-            <img id="flag" src="${country.flags.png}" alt="">
-            <div class="country-card-body">
-              <h2 id="name">${country.name.common}</h2>
-              <h4>Population: <span id="population">${country.population}</span></h4>
-              <h4>Capital: <span id="capital">${country.capital}</span></h4>
-              <h4>Continent: <span id="continent">${country.continents[0]}</span></h4>
-            </div>
-          </div>
-        </div>
-      `
       // Insert populated HTML to main page
-      countries.insertAdjacentHTML("beforeend", countryCard)
+      countries.insertAdjacentHTML("beforeend", createCountryCard(country))
     })
   })
 
+// Select search form
 const form = document.querySelector('#search-box')
+
+// Upon form submission, search API by country name
 form.addEventListener('submit', event => {
   event.preventDefault()
   const input = document.getElementById('search').value
   fetch(`https://restcountries.com/v3.1/name/${input}`)
     .then(response => response.json())
     .then((data) => {
+      // Alert user if country not found
       if(data.status === 404){
         alert("That country doesn't exist! Please try again")
+      } else {
+      // Populate card with data
+        countries.insertAdjacentHTML("beforeend", createCountryCard(data[0]))
       }
     })
 })
