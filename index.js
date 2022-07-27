5c73dd22028e53d7c66d261944c2905be8f8cc53
Borhan Boulandier <borhan.boulandier@gmail.com>
2022-07-27 09:57:23 +0100

    Refactor, added search by country name

--- index.js
+++ index.js
@@ -1,52 +1,69 @@
 // Create country card HTML function
 function createCountryCard(obj) {
-  const element = `
-  <div class="col-3">
-    <div class="country-card my-2">
-      <img id="flag" src="${obj.flags.png}" alt="">
-      <div class="country-card-body">
-        <h2 id="name">${obj.name.common}</h2>
-        <h4>Population: <span id="population">${obj.population}</span></h4>
-        <h4>Capital: <span id="capital">${obj.capital}</span></h4>
-        <h4>Continent: <span id="continent">${obj.continents[0]}</span></h4>
-      </div>
-    </div>
-  </div>
-  `
-  return element;
+  return `<div class="col-3">
+            <div class="country-card my-2">
+              <img id="flag" src="${obj.flag}" alt="">
+              <div class="country-card-body">
+                <h2 id="name">${obj.name}</h2>
+                <h4>Population: <span id="population">${obj.population}</span></h4>
+                <h4>Capital: <span id="capital">${obj.capital}</span></h4>
+                <h4>Continent: <span id="continent">${obj.continent}</span></h4>
+              </div>
+            </div>
+          </div>`
 }
 
 // Select row to insert country cards
 const countries = document.getElementById("countries")
 
-// API call to list all countries
-fetch("https://restcountries.com/v3.1/all")
-  .then(response => response.json())
-  .then((data) => {
+// Create global array of countries
+let countryList = []
 
-    // Iterate through all countries and create HTML populated with country API data
-    Object.values(data).forEach((country) => {
-      // Insert populated HTML to main page
-      countries.insertAdjacentHTML("beforeend", createCountryCard(country))
+// Method to show all countries
+const listAllCountries = () => {
+  // Fetch all countries
+  fetch("https://restcountries.com/v3.1/all")
+    .then(response => response.json())
+    .then((data) => {
+
+      // Create custom objects, add to countryList array
+      countryList = Object.values(data).map(country => {
+        return {
+          flag: country.flags.png,
+          name: country.name.common,
+          population: country.population,
+          capital: country.capital,
+          continent: country.continents[0],
+        }
+      })
+      countryList.forEach((country) => {
+        // Insert populated HTML to main page
+        countries.insertAdjacentHTML("beforeend", createCountryCard(country))
+      })
     })
-  })
+
+}
+// Show all countries upon page load
+window.onload = (event) => {
+  listAllCountries()
+};
 
 // Select search form
 const form = document.querySelector('#search-box')
 
-// Upon form submission, search API by country name
-form.addEventListener('submit', event => {
-  event.preventDefault()
-  const input = document.getElementById('search').value
-  fetch(`https://restcountries.com/v3.1/name/${input}`)
-    .then(response => response.json())
-    .then((data) => {
-      // Alert user if country not found
-      if(data.status === 404){
-        alert("That country doesn't exist! Please try again")
-      } else {
-      // Populate card with data
-        countries.insertAdjacentHTML("beforeend", createCountryCard(data[0]))
-      }
-    })
+// Search countries by name
+form.addEventListener('input', event => {
+
+  // Get user input
+  const value = event.target.value
+  // Filter countries by name, case insensitive
+  const selection = countryList.filter(country => country.name.toLowerCase().includes(value.toLowerCase()))
+
+  // Clear all countries from page
+  countries.innerHTML = ''
+
+  // Insert only filtered countries on page
+  selection.forEach((country) => {
+    countries.insertAdjacentHTML("beforeend", createCountryCard(country))
+  })
 })
